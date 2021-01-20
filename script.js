d1f7b8c2b9cd8d63887bc72f998c3b000bf2339b
jcxiao <chang.x1994@gmail.com>
2021-01-20 18:29:06 -0500

    added modal support for firefox

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const generateBtn = document.querySelector("#generate");
 const submitBtn = document.querySelector("#submitBtn");
 const exitBtn = document.querySelector("#exitBtn");
 
+//polyfill CDN for firefox & safari(?) support
+dialogPolyfill.registerDialog(document.querySelector('#popup'));
+
 // show popup modal
 function showPopUp() {
   document.querySelector('#popup').showModal();
@@ -69,3 +72,22 @@ generateBtn.addEventListener("click", showPopUp);
 submitBtn.addEventListener("click", writePassword);
 exitBtn.addEventListener("click", exitPopup);
 
+function something() {
+
+  'use strict';
+  var dialog = document.querySelector('#modal-example');
+  var closeButton = dialog.querySelector('button');
+  var showButton = document.querySelector('#show-modal-example');
+
+  if (! dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+  }
+  var closeClickHandler = function(event) {
+      dialog.close();
+  };
+  var showClickHandler = function(event) {
+      dialog.showModal();
+  };
+  showButton.addEventListener('click', showClickHandler);
+  closeButton.addEventListener('click', closeClickHandler);
+};
